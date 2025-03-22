22ada8c6959511f79113edca05d55cccdf584db9
Simina Dan-Marius <dansimina10@gmail.com>
2025-03-22 22:46:55 +0200

    commit

--- coding-events/src/main/java/org/example/codingevents/controllers/EventController.java
+++ coding-events/src/main/java/org/example/codingevents/controllers/EventController.java
@@ -1,7 +1,6 @@
 package org.example.codingevents.controllers;
 
 import jakarta.validation.Valid;
-import org.example.codingevents.data.EventData;
 import org.example.codingevents.data.EventsRepository;
 import org.example.codingevents.models.Event;
 import org.example.codingevents.models.EventType;
@@ -22,7 +21,7 @@ public class EventController {
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        model.addAttribute("events", EventData.getAll());
+        model.addAttribute("events", eventsRepository.findAll());
         return "events/index";
     }
 
@@ -51,14 +50,14 @@ public class EventController {
             return "events/create";
         }
 
-        EventData.add(event);
+        eventsRepository.save(event);
         return "redirect:/events"; // return a redirect response which is a 300 level HTTP response and instructs the browser to go to a different page
     }
 
     @GetMapping("delete")
     public String displayDeleteEventForm(Model model) {
         model.addAttribute("title", "Delete Events");
-        model.addAttribute("events", EventData.getAll());
+        model.addAttribute("events", eventsRepository.findAll());
         return "events/delete";
     }
 
@@ -66,7 +65,7 @@ public class EventController {
     public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds) {
         if(eventIds != null) {
             for (int eventId : eventIds) {
-                EventData.remove(eventId);
+                eventsRepository.deleteById(eventId);
             }
         }
 
